ed63d4a750ff0cecd21cb39fe5854df74f9a0b64
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-04-25 16:54:44 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
+let cnt = 0;
 function App() {
+  let s1 = "";
+  const getChunksOfDocuments = async () => {
+    let dataToPost = {
+      document_id: 2,
+      property_id: 1,
+      chunk_number: cnt,
+      chunk_size: 5000,
+    };
+    await axios
+      .post(`/sam/v1/property/auth/property-docs`, dataToPost, {
+        headers: {
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJlbWFpbCI6ImFkbWluQHNhbXRvb2wuY29tIiwiZXhwIjoxNjgyNDIzMTI2LCJyb2xlIjoiQWRtaW4sIiwidXNlcmlkIjoxfQ.sbTmNamYFld4Gdaks6w7ZKQY4AjNic5GIC-Yh6AFkjM",
+        },
+      })
+      .then((res) => {
+        if (res.data.data !== "") {
+          cnt += 1;
+          if (s1 !== res.data.data) {
+            s1 += res.data.data;
+          }
+          getChunksOfDocuments();
+        }
+      });
+  };
+  useEffect(() => {
+    getChunksOfDocuments();
+    // eslint-disable-next-line
+  }, []);
+
+  // const base64 = "";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          console.log(s1);
+        }}
+      >
+        Check
+      </button>
     </div>
   );
 }
