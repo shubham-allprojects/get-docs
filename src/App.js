e13ed5da68e5b1fffdfd38e8bb3e25a190473b07
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-04-28 13:20:05 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
-// pages/index.js
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
   const [maxPages, setMaxPages] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const getPdf = async (pageNumber = 1) => {
+    const pdfJS = await import("pdfjs-dist/build/pdf");
+    pdfJS.GlobalWorkerOptions.workerSrc =
+      window.location.origin + "/pdf.worker.min.js";
+    const pdf = await pdfJS.getDocument("sample.pdf").promise;
+    setMaxPages(pdf._pdfInfo.numPages);
+    const page = await pdf.getPage(pageNumber);
+    const viewport = page.getViewport({ scale: 1.5 });
+    const canvasSample = document.createElement("canvas");
+    let mainDiv = document.getElementById("sample");
+    if (mainDiv.firstChild) {
+      mainDiv.removeChild(mainDiv.firstChild);
+    }
+    mainDiv.appendChild(canvasSample);
+    // Prepare canvas using PDF page dimensions.
+    const canvas = canvasSample;
+    const canvasContext = canvas.getContext("2d");
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context.
+    const renderContext = { canvasContext, viewport };
+    page.render(renderContext);
+  };
+
+  const onNextClick = () => {
+    if (currentPage < maxPages) {
+      setCurrentPage(currentPage + 1);
+      getPdf(currentPage + 1);
+    }
+  };
+
+  const onPrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      getPdf(currentPage - 1);
+    }
+  };
 
   useEffect(() => {
-    (async function () {
-      // We import this here so that it's only loaded during client-side rendering.
-      const pdfJS = await import("pdfjs-dist/build/pdf");
-      pdfJS.GlobalWorkerOptions.workerSrc =
-        window.location.origin + "/pdf.worker.min.js";
-      const pdf = await pdfJS.getDocument("sample.pdf").promise;
-      setMaxPages(pdf._pdfInfo.numPages);
-      const page = await pdf.getPage(1);
-      const viewport = page.getViewport({ scale: 1.5 });
-      const canvasSample = document.createElement("canvas");
-      let mainDiv = document.getElementById("sample");
-      if (mainDiv.firstChild) {
-        mainDiv.removeChild(mainDiv.firstChild);
-      }
-      mainDiv.appendChild(canvasSample);
-      // Prepare canvas using PDF page dimensions.
-      const canvas = canvasSample;
-      const canvasContext = canvas.getContext("2d");
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-
-      // Render PDF page into canvas context.
-      const renderContext = { canvasContext, viewport };
-      page.render(renderContext);
-    })();
+    getPdf(1);
   }, []);
 
   return (
-    <div id="sample">
-      {/* <canvas ref={canvasRef} style={{ height: "100vh" }} /> */}
-    </div>
+    <>
+      <div id="sample">
+        {/* <canvas ref={canvasRef} style={{ height: "100vh" }} /> */}
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={onPrevClick}>
+          Prev
+        </button>
+        currentPage = {currentPage}
+        <button className="btn btn-primary" onClick={onNextClick}>
+          Next
+        </button>
+      </div>
+    </>
   );
 }
